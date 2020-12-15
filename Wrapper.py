88054dac640f3ed8fb844a87f9812c8c956b0bb5
m0rtydump <73741302+rickandm00rty@users.noreply.github.com>
2020-12-15 11:55:02 -0700

    Updated Wrapper with subprocess Python module import
    Still needs testing to run individual commands in subprocesses

--- Wrapper.py
+++ Wrapper.py
@@ -1,2 +1,26 @@
 ## Python wrapper for program
 
+from tkinter import *
+import webbrowser
+import subprocess
+
+#Place standard wireless card in monitoring mode on launch
+EnableMonitor = subprocess.Popen(['airmon-ng start wlan0'], shell=TRUE)
+
+window = Tk()
+
+window.title("Kyle's WiFi PWNER Ver. 1")
+window.geometry('640x100')
+
+txt = Entry(window, width=20)
+txt.grid(column=0, row=1)
+
+def loadDefaults():
+    btn.configure(text="Cracking....")
+    
+
+btn = Button(window, text="Crack my PW", activebackground="Red", command=loadDefaults)
+btn.grid(column=0, row=2)
+
+
+window.mainloop()
