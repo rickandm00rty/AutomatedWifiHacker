10b1d4ffdb9123e149b97f151cad19d76312cc60
m0rtydump <73741302+rickandm00rty@users.noreply.github.com>
2020-12-16 10:50:17 -0700

    Centering, Generate List
    Added Centering for windows and buttons, added a window to take array of AP's scanned

--- Wrapper.py
+++ Wrapper.py
@@ -5,22 +5,31 @@ import webbrowser
 import subprocess
 
 #Place standard wireless card in monitoring mode on launch
-EnableMonitor = subprocess.Popen(['airmon-ng start wlan0'], shell=TRUE)
+subprocess.run(['airmon-ng start wlan0'], shell=TRUE, capture_output=TRUE)
 
+#Generate parent window
 window = Tk()
-
 window.title("Kyle's WiFi PWNER Ver. 1")
-window.geometry('640x100')
+window.geometry('1000x500')
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(0, weight=1)
 
-txt = Entry(window, width=20)
-txt.grid(column=0, row=1)
+#Generate AP List and Display
+APList = Listbox(window, fg="Red", selectbackground='Green', selectmode=BROWSE)
+APList.insert(1, 'APLISTONE')
+APList.insert(2, 'APLISTONE')
+APList.insert(3, 'APLISTONE')
+APList.insert(4, 'APLISTONE')
+APList.grid(row=0, column=2)
+APList.place(relx=0.5, rely=0.5, anchor=CENTER)
 
+#On-Click, run the airodump-ng, write to a file
 def loadDefaults():
     btn.configure(text="Cracking....")
-    
 
+#Generate button, click to 'loadDefaults()'
 btn = Button(window, text="Crack my PW", activebackground="Red", command=loadDefaults)
-btn.grid(column=0, row=2)
-
+btn.grid(row=9, column=0)
+btn.place(relx=0.5, rely=1, anchor=S)
 
 window.mainloop()
